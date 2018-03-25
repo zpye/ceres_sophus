37360e7c8dc5d811bb13e661f450b9c9987cb1fb
ubuntu <ubuntu@localhost.localdomain>
2018-03-25 10:18:34 +0800

    using AutoDiff

---
+++ local_parameterization_se3.hpp
@@ -0,0 +1,28 @@
+#ifndef SOPHUS_LOCAL_PARAMETERIZATION_SE3_HPP
+#define SOPHUS_LOCAL_PARAMETERIZATION_SE3_HPP
+
+#include <ceres/local_parameterization.h>
+#include <sophus/se3.hpp>
+
+namespace Sophus {
+    struct SE3Plus {
+        template<typename T>
+        bool operator()(const T* x, const T* delta, T* x_plus_delta) const {
+            Eigen::Map<SE3<T> const> const T_x(x);
+            Eigen::Map<Vector6<T> const> const T_delta(delta);
+            Eigen::Map<SE3<T>> T_x_plus_delta(x_plus_delta);
+            T_x_plus_delta = SE3<T>::exp(T_delta) * T_x;
+            return true;
+        }
+    };
+
+    ceres::LocalParameterization* GetLocalParamsSE3()
+    {
+        ceres::LocalParameterization* LocalParamSE3 =
+        new ceres::AutoDiffLocalParameterization<Sophus::SE3Plus,
+        Sophus::SE3d::num_parameters, Sophus::SE3d::DoF>;
+
+    }
+}  // namespace Sophus
+
+#endif // SOPHUS_LOCAL_PARAMETERIZATION_SE3_HPP
