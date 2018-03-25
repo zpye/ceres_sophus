50f00d2e8303209932b348bef040bed1aa2fdd2a
NatureBlack <986790855@qq.com>
2018-03-25 11:00:16 +0800

    Delete build.cmd

--- build.cmd
+++
@@ -1,8 +0,0 @@
-rd /q /s .\build
-mkdir build
-cd build
-
-cmake -G"Visual Studio 15 2017 Win64" -DINCLUDE_DIR=E:\SLAM_class\project\3rdparty\include -DLIBRARIES=E:\SLAM_class\project\3rdparty\lib\ceres.lib;E:\SLAM_class\project\3rdparty\lib\libglog_static.lib ..
-
-cmd
- 
